a95b7a1db18359cefd8237bc56eabbba53d07733
Elena <lopeznavarrovidal.elena@gmail.com>
2019-10-18 13:33:59 +0200

    Lee línea por línea para ahorrar memoria

--- palabras-freq.py
+++ palabras-freq.py
@@ -1,7 +1,7 @@
 # Objetivo: este programa lee varios ficheros, dividide las líneas en palabras, calcula el porcentaje de palabras en el texto y muestra una lista.
-# Abrir el fichero, leer todo el contenido y dividir el texto en una lista de palabras.  
+# Abrir el fichero, leer el fichero línea por línea y dividir las líneas en lista de palabras.  
 input_file_path = "sh.txt"
 with open(input_file_path) as f:
-    listaPalabras = f.read().split()
+    listaPalabras = [line.split() for line in f]
 print(listaPalabras)
     
