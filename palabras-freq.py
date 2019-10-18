3c1ed2eb83978a9dff11d0685f4b246e4b50e5db
Elena <lopeznavarrovidal.elena@gmail.com>
2019-10-18 13:46:20 +0200

    Manera más granular de representar que lee por línea por línea

--- palabras-freq.py
+++ palabras-freq.py
@@ -1,7 +1,10 @@
 # Objetivo: este programa lee varios ficheros, dividide las líneas en palabras, calcula el porcentaje de palabras en el texto y muestra una lista.
 # Abrir el fichero, leer el fichero línea por línea y dividir las líneas en lista de palabras.  
 input_file_path = "sh.txt"
+listaPalabras = []
 with open(input_file_path) as f:
-    listaPalabras = [line.split() for line in f]
+    for line in f:
+        line = line.split()
+        listaPalabras.append(line)
 print(listaPalabras)
     
